80aeaf14b8bb9382e24c1cbc174b6e8b3dd5a791
arnoldsky-yu <99588albert@gmail.com>
2024-09-12 23:04:11 +0800

    update

--- web_crawler.py
+++ web_crawler.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # 請調整網址變成你的 Google 試算表網址
-url = "https://docs.google.com/spreadsheets/d/"
-
+url=""
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
@@ -20,5 +19,3 @@ df = pd.DataFrame(rows)
 # 自行調整輸出檔案名稱
 # 註：這個檔案會輸出到你執行這個程式的資料夾
 df.to_csv("spreadsheet_data.csv", index=False)
-print(df)
-
